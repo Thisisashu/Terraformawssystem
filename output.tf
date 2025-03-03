c8948c74e60438645aed2f7023eeff083f1e6718
Thisisashu <ashuagra481990@gmail.com>
2025-03-03 11:37:25 +0530

    added the output file

---
+++ output.tf
@@ -0,0 +1,21 @@
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.subnet1.id
+}
+
+output "subnet_cidr_block" {
+  description = "The CIDR block of the subnet"
+  value       = aws_subnet.subnet1.cidr_block
+}
+
+output "subnet_availability_zone" {
+  description = "The Availability Zone of the subnet"
+  value       = aws_subnet.subnet1.availability_zone
+}
+
+output "subnet_vpc_id" {
+  description = "The VPC ID to which the subnet belongs"
+  value       = aws_subnet.subnet1.vpc_id
+}
+
+
