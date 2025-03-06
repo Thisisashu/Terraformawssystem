79cfb82b578c507b60f9991af42e6db8c4c5cdc3
Thisisashu <ashuagra481990@gmail.com>
2025-03-06 11:48:38 +0530

    updated the codes

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 resource "aws_instance" "my_vm"  { 
+count  = 2
 ami = data.aws_ami.latest_amazon_linux.id
 instance_type = var.instance_type 
 subnet_id = data.terraform_remote_state.network_details.outputs.subnet_id 
@@ -6,19 +7,26 @@ key_name = data.terraform_remote_state.network_details.outputs.aws_key_pair
 depends_on = [data.terraform_remote_state.network_details]
 security_groups = [
     data.terraform_remote_state.network_details.outputs.security_group_id  # Security Group ID from remote state
-  ] 
+  ]
+lifecycle {
+    ignore_changes = [security_groups] #Ignore security group changes
+  #    create_before_destroy = true
+    prevent_destroy       = true
+
+  }
+ 
  
 tags = {
-  Name = format("MyInstance-%s", formatdate("YYYYMMDD", timestamp()))
+  Name = "student.19-vm${count.index + 1}"
 }
   
 }
 output "instance_public_ip" {
   description = "The public IP address of the EC2 instance"
-  value       = aws_instance.my_vm.public_ip
+  value       = aws_instance.my_vm[*].public_ip
 }
 
 output "instance_type" {
- value = aws_instance.my_vm.instance_type
+ value = aws_instance.my_vm[*].instance_type
 }
 
